160736d41ef4214bad08a064fc3c0255d6949190
animevn <haanhth21@gmail.com>
2020-09-23 21:10:21 +0700

    reset

--- app/src/main/java/com/haanhgs/app/bmi/view/MainActivity.java
+++ app/src/main/java/com/haanhgs/app/bmi/view/MainActivity.java
@@ -1,72 +1,16 @@
 package com.haanhgs.app.bmi.view;
 
 import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import com.haanhgs.app.bmi.R;
-import com.haanhgs.app.bmi.viewmodel.view.ViewModel;
-import java.util.Locale;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.textView)
-    TextView textView;
-    @BindView(R.id.textView3)
-    TextView textView3;
-    @BindView(R.id.textView4)
-    TextView textView4;
-    @BindView(R.id.etWeight)
-    EditText etWeight;
-    @BindView(R.id.etHeight)
-    EditText etHeight;
-    @BindView(R.id.tvBMI)
-    TextView tvBMI;
-    @BindView(R.id.tvResult)
-    TextView tvResult;
-    @BindView(R.id.bnCalculate)
-    Button bnCalculate;
-
-    private ViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-        viewModel = new ViewModelProvider(this).get(ViewModel.class);
-        viewModel.getLiveData().observe(this, bmi -> {
-            if (bmi.getBmi() == null){
-                tvBMI.setText("");
-            }else {
-                tvBMI.setText(String.format(Locale.getDefault(), "%.2f", bmi.getBmi()));
-            }
-            tvResult.setText(bmi.getBmiCategory());
-        });
+
     }
 
-    private void hideKeboard(){
-        InputMethodManager manager = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
-        View view = this.getCurrentFocus();
-        if (manager != null && view != null){
-            manager.hideSoftInputFromWindow(
-                    view.getWindowToken(),
-                    InputMethodManager.HIDE_NOT_ALWAYS);
-        }
-    }
 
-    @OnClick(R.id.bnCalculate)
-    public void onViewClicked() {
-        hideKeboard();
-        String weight = etWeight.getText().toString();
-        String height = etHeight.getText().toString();
-        viewModel.calculateBmi(weight, height);
-    }
+
 }
--- app/src/main/java/com/haanhgs/app/bmi/viewmodel/view/ViewModel.java
+++ app/src/main/java/com/haanhgs/app/bmi/viewmodel/view/ViewModel.java
@@ -1,48 +1,6 @@
 package com.haanhgs.app.bmi.viewmodel.view;
 
-import android.app.Application;
-import com.haanhgs.app.bmi.model.Bmi;
-import androidx.annotation.NonNull;
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.MutableLiveData;
+public class ViewModel extends androidx.lifecycle.ViewModel {
 
-public class ViewModel extends AndroidViewModel {
-
-    private final MutableLiveData<Bmi> liveData = new MutableLiveData<>();
-
-    public ViewModel(@NonNull Application application) {
-        super(application);
-        liveData.setValue(new Bmi());
-    }
-
-    public MutableLiveData<Bmi> getLiveData() {
-        return liveData;
-    }
-
-    private static boolean isFloat(String string){
-        try{
-            return Float.parseFloat(string) > 0;
-        }catch (NumberFormatException e){
-            return false;
-        }
-    }
-
-    public void calculateBmi(String weightStr, String heightStr) {
-        if (liveData.getValue() != null){
-            Bmi bmi = liveData.getValue();
-            if (isFloat(weightStr) && isFloat(heightStr)){
-                float weight = Float.parseFloat(weightStr);
-                float height = Float.parseFloat(heightStr);
-                bmi.setHeight(height);
-                bmi.setWeight(weight);
-                bmi.calBmi();
-                bmi.calBmiCategory();
-            }else {
-                bmi.setBmi(null);
-                bmi.setBmiCategory("Weight and Height cannot be zero");
-            }
-            liveData.setValue(bmi);
-        }
-    }
 
 }
